9d811d7e566fe78c6a8890909b4728e80b87871d
saadkhan29 <sunny_khan_29@yahoo.com>
2025-07-27 14:50:56 +0300

    Hoot Frontend

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import NavBar from './components/NavBar/NavBar'
+import {Routes, Route} from 'react-router'
+import SignUpForm from './components/SignUpForm/SignUpForm'
+import SignInForm from './components/SignInForm/SignInForm'
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+import { UserContext } from './contexts/UserContext';
+
+const App = () => {
+  const { user } = useContext(UserContext);
+
+  return (
+    <>
+    <NavBar />
+          <Routes>
+        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
+        {user ? (
+          <>
+            {/* Protected routes (available only to signed-in users) */}
+          </>
+        ) : (
+          <>
+            {/* Non-user routes (available only to guests) */}
+            <Route path='/sign-up' element={<SignUpForm />} />
+            <Route path='/sign-in' element={<SignInForm />} />
+          </>
+        )}
+      </Routes>
+    </>
+  )
+}
+
+export default App
